81a25bab55d4f4c628aeda3d51406055e9d3b552
shubham sharma <shubham00sharma0@gmail.com>
2023-04-03 18:00:36 +0530

    first commit

---
+++ src/test/java/com/noobdev/dailyblog/dailyblog/DailyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobdev.dailyblog.dailyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
